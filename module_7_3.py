06bbecf7f305c080913228e83c745a6743f9ae7f
Aleksey_Abramenko <x903xxxx@gmail.com>
2025-01-15 20:11:12 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -24,19 +24,24 @@ class WordsFinder:
         finder2.get_all_words()
         for name, list_ in all_words.items():
             for i in list_:
-                if str(i) == word.lower:
-                    new_dict[name] = i
-        print(new_dict)
-        print(name)
-        print(i)
+                if str(i) == word.lower():
+                    new_dict[name] = list_.index(i) + 1
         return new_dict
 
 
+    def count(self, word):
+        new_dict = {}
+        count = 0
+        finder2.get_all_words()
+        for name, list_ in all_words.items():
+            for i in list_:
+                if str(i) == word.lower():
+                    count += 1
+                    new_dict[name] = count
+        return new_dict
 
 
 finder2 = WordsFinder('test_file.txt')
-
-print(finder2.get_all_words())
-print(finder2.find('text'))
-
-print (all_words.values())
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT'))
